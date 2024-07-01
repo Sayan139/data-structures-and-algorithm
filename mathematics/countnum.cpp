db4be376773e7d449f39ae62a5d43b6ede5e7eb7
Sayan Biswas <143019848+Sayan139@users.noreply.github.com>
2024-07-01 11:05:06 +0530

    code of count the number of digits

--- mathematics/countnum.cpp
+++ mathematics/countnum.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+int countdigits(int n){
+    int res=0;
+    while(n>0){
+        n=n/10;
+        res++;
+    }
+    return res;
+}
+int main(){
+    int n;
+    cout << "enter the number you want the num of digits: ";
+    cin >> n;
+    cout << "the number of digits " << n << " have : " << countdigits(n);
+}
