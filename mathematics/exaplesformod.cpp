d4c4fe447a52990b74b3b742c3b2e2a2353596d0
Sayan Biswas <143019848+Sayan139@users.noreply.github.com>
2024-08-03 11:58:33 +0530

    del

--- mathematics/exaplesformod.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a=10;
-    int b=20;
-    int mod = 7;
-    cout << "modular addition : " << ((a%mod)+(b%mod))%mod << endl;
-    cout << "modular multiplication : " << ((a%mod)*(b%mod))%mod << endl;
-}
