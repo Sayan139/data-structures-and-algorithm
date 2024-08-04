7af0bf94de1ee91ed982e3117f4704280a64806d
Sayan Biswas <143019848+Sayan139@users.noreply.github.com>
2024-08-04 23:32:29 +0530

    4 aug

---
+++ patterns.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+void printpattern1(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout << "* " ;
+        }
+        cout << endl;
+    }
+}
+void printpattern2(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << "* " ;
+        }
+        cout << endl;
+    }
+}
+void printpattern3(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i+1; j++){
+            cout << " ";
+        }
+        for(int j =0; j<2*i+1; j++){
+            cout << "*";
+        }
+        for(int j=0; j<n-i+1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    int n;
+    cout << "enter the number: ";
+    cin >> n;
+    printpattern1(n);
+    printpattern2(n);
+    printpattern3(n);
+}
