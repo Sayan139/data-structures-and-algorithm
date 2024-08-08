fa1d00698112487fef03fbfec3ebe4e66b12f026
Sayan Biswas <143019848+Sayan139@users.noreply.github.com>
2024-08-08 23:51:50 +0530

    aug 8

--- patterns.cpp
+++ patterns.cpp
@@ -17,29 +17,115 @@ void printpattern2(int n){
     }
 }
 void printpattern3(int n){
-    for(int i=0; i<n; i++){
-        for(int j=2; j<n-i+1; j++){
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<n; j++){
             cout << " ";
         }
-        for(int j =0; j<2*i+1; j++){
+        for(int j=1; j<=2*i-1; j++){
             cout << "*";
-        }
-        for(int j=0; j<n-i+1; j++){
-            cout << " ";
         }
         cout << endl;
     }
 }
 void printpattern4(int n){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<i; j++){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<i; j++){
             cout << " ";
         }
-        for(int j =0; j<2*n-(2*i+1); j++){
+        for(int j=1; j<=2*n-(2*i-1); j++){
             cout << "*";
         }
-        for(int j=0; j<i; j++){
+        cout << endl;
+    }
+}
+void printpattern5(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=1; i<n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void printpattern6(int n){
+    for(int i=0; i<n; i++){
+        int start =0;
+        if(i%2==0) start =1;
+        else start =0;
+        for(int j=0; j<=i; j++){
+            cout << start << " ";
+            start = 1 - start;
+        }
+        cout << endl;
+    }
+}
+void printpattern7(int n){
+    int space = 2 * (n-1);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << j << " ";
+        }
+        for(int j=1; j<=2 * space; j++){
             cout << " ";
+        }
+        for(int j=i; j>=1; j--){
+            cout << j << " ";
+        }
+        cout << endl;
+        space -= 2;
+    }
+}
+void printpattern8(int n){
+    int start = 2 * (n-1);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << j << " ";
+        }
+        for(int j=1; j<=start; j++){
+            cout << " " ;
+        }
+        for(int j=i; j>=1; j--){
+            cout << j << " ";
+        }
+        cout << endl;
+        start -= 2;
+    }
+}
+void printpattern9(int n){
+    int start =1;
+    for(int i=1; i<=n;i++){
+        for(int j=1; j<=i; j++){
+            cout << start << " ";
+            start ++;
+        }
+        cout << endl;
+    }
+}
+void printpattern10(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << (char)(64+j) << " ";
+        }
+        cout << endl;
+    }
+}
+void printpattern11(int n){
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            cout << (char)(64+j) << " ";
+        }
+        cout << endl;
+    }
+}
+void printpattern12(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << (char)(64+i) << " ";
         }
         cout << endl;
     }
@@ -48,6 +134,5 @@ int main(){
     int n;
     cout << "enter the number: ";
     cin >> n;
-    printpattern3(n);
-    printpattern4(n);
+    printpattern12(n);
 }
